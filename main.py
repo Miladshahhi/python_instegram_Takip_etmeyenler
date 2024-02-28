bc34226ddb1d91380f34f8bf6a7c647ab1d027fd
Hakkı <75142464+hakkiodbs@users.noreply.github.com>
2024-02-29 01:09:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import instaloader
+
+L = instaloader.Instaloader()
+
+username = "kullanıcıAdı"
+password = "Şifreniz"
+L.login(username, password)  # (login)
+
+profile = instaloader.Profile.from_username(L.context, username)
+
+# Takipçileri al
+followers_file = "takipci.txt"
+with open(followers_file, "w") as f:
+    for follower in profile.get_followers():
+        f.write(follower.username + '\n')
+    print("Takipçiler yazıldı.")
+
+# Takip edilenleri al
+followees_file = "takipedilen.txt"
+with open(followees_file, "w") as f:
+    for followee in profile.get_followees():
+        f.write(followee.username + '\n')
+    print("Takip edilenler yazıldı.")
+
+# Takip etmeyenleri bul
+followers_set = set(open(followers_file).readlines())
+followees_set = set(open(followees_file).readlines())
+
+unfollowers_set = followees_set.difference(followers_set)
+
+# Takip etmeyenleri txt dosyasına yaz
+unfollowers_file = "takip_etmeyenler.txt"
+with open(unfollowers_file, "w") as f:
+    for unfollower in unfollowers_set:
+        f.write(unfollower)
+    print("Takip etmeyenler dosyaya yazıldı.")
